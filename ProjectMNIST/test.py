0e3b5d02380c4ed7c3cd31b1b64c58d19e2867a3
Minho Park <iampmh0601@gmail.com>
2020-02-15 00:33:23 +0900

    Training set and Test set

--- ProjectMNIST/test.py
+++ ProjectMNIST/test.py
@@ -1,9 +1,31 @@
 import os
 import sys
+
+sys.path.append(os.pardir)
 import numpy as np
 import pickle
 from common.functions import sigmoid, softmax
 from data_mnist.mnist import load_mnist
 from PIL import Image
 
-sys.path.append(os.pardir)
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+
+# print(x_train.shape)
+# print(t_train.shape)
+# print(x_test.shape)
+# print(t_test.shape)
+
+
+def img_show(img):
+    pil_img = Image.fromarray(np.uint8(img))
+    pil_img.show()
+
+
+img = x_train[35]
+label = t_train[35]
+print(img.shape)
+img = img.reshape(28, 28)
+print(img.shape)
+
+img_show(img)
